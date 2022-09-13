7a420ee2f736f61915aa832d0c56ea0c9bc9fcd1
killshotxd <hassanansari211@gmail.com>
2022-09-13 10:45:14 +0530

    Upload wall and getting url

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -58,4 +58,60 @@ const getUserFromDb = async (uid) => {
   return result.data();
 };
 
-export { auth, storage, updateUserDb, getUserFromDb };
+// ------------Upload Image to FB---------------
+
+const uploadImage = (file, progressCallback, urlCallback, errorCallback) => {
+  if (!file) {
+    errorCallback("File not found");
+    return;
+  }
+
+  const fileType = file.type;
+  const fileSize = file.size / 1024 / 1024;
+
+  if (!fileType.includes("image")) {
+    errorCallback("File must an image");
+    return;
+  }
+  if (fileSize > 2) {
+    errorCallback("File must smaller than 2MB");
+    return;
+  }
+
+  const storageRef = ref(storage, `images2/${file.name}`);
+
+  const task = uploadBytesResumable(storageRef, file);
+
+  task.on(
+    "state_changed",
+    (snapshot) => {
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      progressCallback(progress);
+    },
+    (error) => {
+      errorCallback(error.message);
+    },
+    () => {
+      getDownloadURL(storageRef).then((url) => {
+        urlCallback(url);
+      });
+    }
+  );
+};
+
+// --------------------Add Projects----------------
+
+const addProjectsInDb = async (project) => {
+  if (typeof project !== "object") return;
+  const collectionRef = collection(db, "projects");
+  await addDoc(collectionRef, { ...project });
+};
+
+export {
+  auth,
+  storage,
+  updateUserDb,
+  getUserFromDb,
+  uploadImage,
+  addProjectsInDb,
+};
--- src/components/Accounts/Account.jsx
+++ src/components/Accounts/Account.jsx
@@ -1,19 +1,87 @@
 import React, { useEffect, useRef, useState } from "react";
 import styles from "./Account.module.css";
-import { storage } from "../../Firebase";
-import { Button } from "@chakra-ui/react";
+import { addProjectsInDb, auth, storage, uploadImage } from "../../Firebase";
+import { Button, Input } from "@chakra-ui/react";
 const Account = (props) => {
+  const fileInputRef = useRef();
   const userDetails = props.userDetails;
   const isAuth = props.auth;
 
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [imageUploadStarted, setImageUploadStarted] = useState(false);
+
+  const [imageUploadProgress, setImageUploadProgress] = useState(0);
+  const [submitButtonDisabled, setSetSubmitButtonDisabled] = useState(false);
   const [values, setValues] = useState({
     name: userDetails.name || "",
+    title: "",
+    thumbnail: "",
   });
+
+  const handleFileInputChange = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    setImageUploadStarted(true);
+    uploadImage(
+      file,
+      (progress) => {
+        setImageUploadProgress(progress);
+      },
+      (url) => {
+        setImageUploadStarted(false);
+        setImageUploadProgress(0);
+        setValues((prev) => ({ ...prev, thumbnail: url }));
+      },
+      (error) => {
+        setImageUploadStarted(false);
+        setErrorMessage(error);
+      }
+    );
+  };
+
+  const handleSubmission = async () => {
+    setSetSubmitButtonDisabled(true);
+    if (auth) {
+      await addProjectsInDb({ ...values });
+    }
+    setSetSubmitButtonDisabled(false);
+    if (props.onSubmission) props.onSubmission();
+    if (props.onClose) props.onClose();
+  };
 
   return (
     <div className={styles.container}>
+      <input
+        ref={fileInputRef}
+        type="file"
+        style={{ display: "none" }}
+        onChange={handleFileInputChange}
+      />
       <div className={styles.btn}>
-        <Button variant="ghost">Upload</Button>
+        <Input
+          placeholder="Enter Title"
+          value={values.title}
+          onChange={(event) =>
+            setValues((prev) => ({
+              ...prev,
+              title: event.target.value,
+            }))
+          }
+        />
+        <Button onClick={() => fileInputRef.current.click()} variant="ghost">
+          Upload Image
+        </Button>
+        {imageUploadStarted && (
+          <p>
+            <span>{imageUploadProgress.toFixed(2)}%</span> Uploaded...
+          </p>
+        )}
+
+        <Button onClick={handleSubmission} disabled={submitButtonDisabled}>
+          Submit
+        </Button>
       </div>
     </div>
   );
